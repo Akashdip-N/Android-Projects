ecb925114de3b06343bc897c33f074e51743a44e
Akashdip-N <Akashdipneogi@gmail.com>
2023-07-13 21:43:35 +0530

    Added few more projects

--- LittleLemon/app/src/main/java/com/littlelemoningredients/littlelemon/ui/theme/Theme.kt
+++ LittleLemon/app/src/main/java/com/littlelemoningredients/littlelemon/ui/theme/Theme.kt
@@ -1,4 +1,4 @@
-package com.example.littlelemon.ui.theme
+package com.littlelemoningredients.littlelemon.ui.theme
 
 import android.app.Activity
 import android.os.Build
--- LittleLemon/app/src/test/java/com/littlelemoningredients/littlelemon/ExampleUnitTest.kt
+++ LittleLemon/app/src/test/java/com/littlelemoningredients/littlelemon/ExampleUnitTest.kt
@@ -1,4 +1,4 @@
-package com.example.littlelemon
+package com.littlelemoningredients.littlelemon
 
 import org.junit.Test
 
