d902282f148faa8a9f906be13596858185b6774a
Akashdip-N <Akashdipneogi@gmail.com>
2023-08-29 12:57:22 +0530

    Commit

--- C7M5 Graded assessment start/app/src/main/java/com/example/littlelemon/MainActivity.kt
+++ C7M5 Graded assessment start/app/src/main/java/com/example/littlelemon/MainActivity.kt
@@ -4,25 +4,37 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material.Button
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
+import androidx.compose.material.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.lifecycleScope
 import androidx.room.Room
 import com.example.littlelemon.ui.theme.LittleLemonTheme
 import io.ktor.client.HttpClient
-import io.ktor.client.call.body
 import io.ktor.client.engine.android.Android
 import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
 import io.ktor.client.request.get
@@ -42,6 +54,7 @@ class MainActivity : ComponentActivity() {
     private val database by lazy {
         Room.databaseBuilder(applicationContext, AppDatabase::class.java, "database").build()
     }
+    private val responseLiveData = MutableLiveData<List<String>>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -53,6 +66,9 @@ class MainActivity : ComponentActivity() {
 
                 // add menuItems variable here
 
+                var databaseMenuItems = database.menuItemDao().getAll()
+
+                val responseState = responseLiveData.observeAsState("").value
                 Column(
                     modifier = Modifier
                         .padding(16.dp)
@@ -70,8 +86,41 @@ class MainActivity : ComponentActivity() {
                     // Add OutlinedTextField
 
                     // add is not empty check here
-
-
+                    Button(
+                        modifier = Modifier
+                            .padding(
+                                100.dp,
+                                0.dp,
+                                0.dp,
+                                0.dp
+                            ),
+                        onClick = {
+                            lifecycleScope.launch{
+                                val response = fetchMenu()
+                                responseLiveData.postValue(listOf(response))
+                            }
+                        }
+                    ) {
+                        Text(text = "Tap Order by Name")
+                    }
+                    var search by remember { mutableStateOf("") }
+                    TextField(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(16.dp)
+                            .background(Color.White)
+                            .border(1.dp, Color.Yellow, shape = MaterialTheme.shapes.medium),
+                        singleLine = true
+                        ,
+                        value = search,
+                        onValueChange = {
+                            value -> search = value
+                        },
+                        label = { Text("Search") }
+                    )
+                    Text(
+                        text = responseState.toString()
+                    )
                 }
             }
         }
@@ -79,16 +128,16 @@ class MainActivity : ComponentActivity() {
         lifecycleScope.launch(Dispatchers.IO) {
             if (database.menuItemDao().isEmpty()) {
                 val menu = fetchMenu()
-                val menuItemsNetwork = parseMenu(menu)
-                saveMenuToDatabase(menuItemsNetwork)
+                // val menuItemsNetwork = parseMenu(menu)
+                // saveMenuToDatabase(menuItemsNetwork)
             }
         }
     }
 
-    private suspend fun fetchMenu(): List<MenuItemNetwork> {
+    private suspend fun fetchMenu(): String {
         return httpClient
             .get("https://raw.githubusercontent.com/Meta-Mobile-Developer-PC/Working-With-Data-API/main/littleLemonSimpleMenu.json")
-            .body()
+            .bodyAsText()
     }
 
     private fun saveMenuToDatabase(menuItemsNetwork: List<MenuItemNetwork>) {
--- C7M5 Graded assessment start/app/src/main/java/com/example/littlelemon/Network.kt
+++ C7M5 Graded assessment start/app/src/main/java/com/example/littlelemon/Network.kt
@@ -5,15 +5,12 @@ import kotlinx.serialization.Serializable
 
 @Serializable
 data class MenuNetwork(
-
     @SerialName("menu")
     val menu: List<MenuItemNetwork>,
-
-    )
+)
 
 @Serializable
 data class MenuItemNetwork(
-
     @SerialName("id")
     val id: Int,
 
@@ -22,8 +19,7 @@ data class MenuItemNetwork(
 
     @SerialName("price")
     val price: Double,
-
-    ){
+) {
     fun toMenuItemRoom() = MenuItemRoom(
         id,
         title,
